7c6184192167d660a9fd6e3a27e5db5ce32928e5
insoo00 <insoosos1234@gmail.com>
2025-01-07 13:39:33 +0900

    first commit

---
+++ src/main/java/hana/ro/ending_credits/EndingCreditsApplication.java
@@ -0,0 +1,13 @@
+package hana.ro.ending_credits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndingCreditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndingCreditsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hana/ro/ending_credits/EndingCreditsApplicationTests.java
@@ -0,0 +1,13 @@
+package hana.ro.ending_credits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingCreditsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
